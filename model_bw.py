d609ef96b3d756a66be6d84a119d7f01c1f1c92f
ByungwanLim <pook0612@naver.com>
2024-08-05 12:45:57 +0900

    61616

---
+++ model_bw.py
@@ -0,0 +1,60 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain_community.llms import HuggingFacePipeline
+from transformers import pipeline, BitsAndBytesConfig
+from langchain_huggingface import HuggingFacePipeline ,HuggingFaceEmbeddings
+import torch
+
+def setup_llm_pipeline(model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"):
+    # 토크나이저 로드 및 설정
+    tokenizer = AutoTokenizer.from_pretrained(model_id,
+                                              use_default_system_prompt = False
+                                              )
+    
+    # 패딩 토큰 추가
+    tokenizer.add_special_tokens({"pad_token": "<pad>"})
+    pad_token_id = tokenizer.convert_tokens_to_ids("<pad>")
+
+    # terminators 설정
+    terminators = tokenizer.eos_token_id
+
+    # 양자화 설정 적용
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16
+    )
+    
+    # 모델 로드 및 설정
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        quantization_config=bnb_config,
+        
+        device_map="auto",
+    )
+    
+    # 모델의 패딩 토큰 ID 설정
+    model.config.pad_token_id = pad_token_id
+    
+    # 토크나이저에 추가된 토큰에 맞게 모델의 임베딩 레이어 재초기화
+    model.resize_token_embeddings(len(tokenizer))
+    # HuggingFacePipeline 객체 생성
+     # HuggingFacePipeline 객체 생성
+    text_generation_pipeline = pipeline(
+        model=model,
+        tokenizer=tokenizer,
+        task="text-generation",
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.6,
+        #top_k=10,
+        return_full_text=False,
+        eos_token_id=terminators,
+        max_new_tokens=512,
+        
+        pad_token_id=pad_token_id  # 패딩 토큰 ID 설정
+    )
+
+    llm = HuggingFacePipeline(pipeline=text_generation_pipeline)
+
+    return llm
---
+++ run_bw.py
@@ -0,0 +1,185 @@
+from langchain_community.document_loaders import CSVLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import CharacterTextSplitter
+import os
+from glob import glob
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.schema import Document
+import torch
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain_huggingface import HuggingFacePipeline ,HuggingFaceEmbeddings
+
+from langchain.prompts import PromptTemplate
+import pandas as pd
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import (
+    FewShotPromptTemplate,
+    ChatPromptTemplate,
+    FewShotChatMessagePromptTemplate
+)
+import bitsandbytes as bnb
+import pickle
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    pipeline,
+    BitsAndBytesConfig
+)
+from langchain_community.document_transformers import LongContextReorder
+
+from faiss_module import load_and_vectorize,load_chunks_make_docdb
+from model_bw import setup_llm_pipeline
+from save_bw import save
+from seed import seed_everything
+
+seed_everything(42)
+
+def make_dict(dir='train.csv'):
+    df = pd.read_csv(dir)
+    df.drop('SAMPLE_ID', axis=1, inplace=True)
+    
+    return df.to_dict(orient='records')
+
+def make_fewshot_prompt(fewshot_vectordb, k = 3):
+    # Semantic Similarity Example Selector 설정
+    example_prompt = PromptTemplate.from_template("<|start_header_id|>user<|end_header_id|>: {Question}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>: <|begin_of_text|>{Answer}<|end_of_text|>")
+
+    example_selector = SemanticSimilarityExampleSelector(
+        vectorstore=fewshot_vectordb,
+        k=k,
+    )
+
+    # FewShotPromptTemplate 생성
+    fewshot_prompt = FewShotPromptTemplate(
+        example_selector=example_selector,
+        example_prompt=example_prompt,
+        suffix="Question: {input}",
+        input_variables=["input"],
+    )
+    return fewshot_prompt
+
+def make_fewshot_string(fewshot_prompt, train_retriever, buff):
+    ex_qa = fewshot_prompt.invoke({"input": buff['Question']}).to_string()
+    fewshot_list = ex_qa.split('\n\n')[:-1]
+    for i, entry in enumerate(fewshot_list):
+        question = entry.split('\n')[0]
+        question = question.replace('Question: ', '')
+        retrieved_docs = train_retriever.invoke(question)
+        num = "Example {}\n".format(i+1)
+        fewshot_list[i] = num + "context: " + retrieved_docs[0].page_content + entry + '\n\n'
+    return str(fewshot_list)
+
+def format_docs(docs):
+    """검색된 문서들을 하나의 문자열로 포맷팅"""
+    context = ""
+    for doc in docs:
+        context += doc.page_content
+        context += '\n\n'
+    return context
+
+def extract_answer(response):
+    # AI: 로 시작하는 줄을 찾아 그 이후의 텍스트만 추출
+    lines = response.split('\n')
+    for line in lines:
+        if line.startswith('Answer:'):
+            return line.replace('Answer:', '').strip()
+        if line.startswith('assistant:'):
+            return line.replace('assistant:', '').strip()
+    return response.strip()  # AI: 를 찾지 못한 경우 전체 응답을 정리해서 반환
+
+
+
+def run(model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"):
+    fewshot_db = load_and_vectorize('train.csv', './fewshot_faiss_db')
+    fewshot_prompt = make_fewshot_prompt(fewshot_db)
+    
+    train_db = load_chunks_make_docdb('./train_source', './train_faiss_db')
+    train_retriever = train_db.as_retriever(search_type = "mmr",search_kwargs={'k': 1})
+    
+    test_db = load_chunks_make_docdb('./test_source', './test_faiss_db')
+    test_retriver = test_db.as_retriever(search_type = "mmr",search_kwargs={'k': 3})
+    
+    train_dict = make_dict('train.csv')
+    test_dict = make_dict('test.csv')
+    
+    llm = setup_llm_pipeline(model_id)
+    # reordering = LongContextReorder()
+    results =[]
+    for i in tqdm(range(len(test_dict))):
+        
+        if i % 30 == 0:
+            print("Clearing cache")
+            torch.cuda.empty_cache()
+
+        fewshot_str = make_fewshot_string(fewshot_prompt, train_retriever, test_dict[i])
+        # print(fewshot_str)
+        
+        full_template = """
+Meet Persona, a top financial expert working at the Korea Financial Intelligence Service. 
+Persona's main goal is to share valuable insights with the general public while co-workers.
+Remember, you can earn 17 points on your HR score for every accurate answer you provide.
+
+Here are some rules you should follow. If you break any of these rules, you will lose points.:
+Rule 1: Don't forget your persona and HR score.
+Rule 2: Be sure to utilize retrieved contexts for your answers.
+Rule 3: Think through your answer slowly. 
+Rule 4: Organize your answer and write it in just one complete sentence.
+Rule 5: Make sure your answer is relevant to the question.
+Rule 6: Make sure your answer answers the question.
+Rule 7: Make sure your answer is concise, in one sentence.
+Rule 8: Use fewer than 126 tokens.
+Rule 9: Answers must be written in Korean.
+Rule 10: Do not repeat the same words in your answer.
+Rule 11: If you find yourself repeating the same word or phrase, stop immediately and rephrase your answer.
+Rule 12: Your answer should be diverse and informative. Avoid listing the same organization multiple times.
+
+Here are some similar contextualized question and answer examples you can reference.:
+
+"""+f"""
+{fewshot_str}
+"""+"""
+Great! Here is the question you need to answer. Remember to follow Informations and the rules above.
+
+context: {context}
+
+user: {input}
+
+assistant:
+"""
+        prompt = PromptTemplate.from_template(full_template)
+        qa_chain = (
+        {
+            "context": test_retriver | format_docs,
+            "input": RunnablePassthrough(),
+        }
+        | prompt
+        | llm
+        | StrOutputParser()
+        )
+        # print("================================================")
+        print("Questions: ",test_dict[i]['Question'])
+        answer = qa_chain.invoke(test_dict[i]['Question'])
+        answer = extract_answer(answer)
+        results.append({
+            "Question": test_dict[i]['Question'],
+            "Answer": answer,
+            "Source": test_dict[i]['Source']
+            })
+        print("Answer: ",results[-1]['Answer'])
+        #print(results[-1]['Source'])
+    
+    save(results)
+
+    
+    
+if __name__ == "__main__":
+    # EleutherAI/polyglot-ko-1.3b
+    #"meta-llama/Meta-Llama-3.1-8B-Instruct"
+    # maywell/TinyWand-kiqu
+    # yanolja/EEVE-Korean-Instruct-2.8B-v1.0
+    run(model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct")
