2a1008c01288ed4f27840019df1c6af75ee00e97
wooseungw <woow070840@gmail.com>
2024-08-09 00:52:09 +0900

    2

--- fewshot_module.py
+++ fewshot_module.py
@@ -12,7 +12,7 @@ def fewshot_ex(fewshot_db, buff, train_retriever = None, fewshot_num = 3):
         vectorstore=fewshot_db,
         k=fewshot_num,
     )
-    ex = example_selector.select_examples({'Question': buff['Question']})
+    ex = example_selector.select_examples({'Question': buff['Question']}) # buff['Question']에 해당하는 fewshot_num개의 문서를 선택 리턴: [{'Question': '질문', 'Answer': '답변'}]
     fewshot_str = ""
     for i , entry in enumerate(ex):
         question = entry['Question']
@@ -20,8 +20,8 @@ def fewshot_ex(fewshot_db, buff, train_retriever = None, fewshot_num = 3):
             fewshot_prompt = PromptTemplate.from_template("""<|start_header_id|>system<|end_header_id|>
 Given the following contexts about Question:\n{contents}"""+template)
             retrieved_docs = train_retriever.invoke(question)
-            ex[i]['contents'] = format_docs(retrieved_docs)
+            ex[i]['contents'] = format_docs(retrieved_docs) # [{'Question': '질문', 'Answer': '답변', 'contents': '문서'}]
         else:
-            fewshot_prompt = PromptTemplate.from_template(template)
-        fewshot_str += fewshot_prompt.format(**ex[i])
+            fewshot_prompt = PromptTemplate.from_template(template) # [{'Question': '질문', 'Answer': '답변'}]
+        fewshot_str += fewshot_prompt.format(**ex[i]) 
     return fewshot_str
