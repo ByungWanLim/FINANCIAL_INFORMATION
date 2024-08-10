be2a8f2c0ea7e57b2c756a3b9b54f51084ca0fab
ByungWanLim~ <pook0612@naver.com>
2024-08-10 18:04:26 +0900

    163163

---
+++ faiss_module_bw.py
@@ -0,0 +1,142 @@
+from langchain_community.document_loaders import CSVLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import CharacterTextSplitter
+import os
+from glob import glob
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.schema import Document
+import torch
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain_community.llms import HuggingFacePipeline
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.prompts import PromptTemplate
+import pandas as pd
+from langchain_core.prompts import (
+    FewShotPromptTemplate
+)
+import pickle
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+import unicodedata
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chat_models import ChatOpenAI  # 또는 다른 적절한 LLM
+
+def create_multi_query_retriever(db, llm, k=3):
+    retriever = MultiQueryRetriever.from_llm(
+        retriever=db.as_retriever(search_kwargs={'k': k}),
+        llm=llm
+    )
+    return retriever
+
+def normalize_string(s):
+    """유니코드 정규화"""
+    return unicodedata.normalize('NFC', s)
+
+
+def get_embedding():
+    embeddings = HuggingFaceEmbeddings(
+        model_name='intfloat/multilingual-e5-small',
+        model_kwargs={'device': 'cuda'},
+        encode_kwargs={'normalize_embeddings': True}
+        )
+    return embeddings
+
+def load_and_vectorize(csv_path, db_path):
+    if os.path.exists(db_path) and os.path.exists(db_path + '_metadata.pkl'):
+        print("Loading FAISS DB from:", db_path)
+        db, metadata = load_faiss_db(db_path)
+        return db
+
+    # CSV 파일을 불러와 데이터프레임 생성
+    train_df = pd.read_csv(csv_path)
+    train_df.drop('SAMPLE_ID', axis=1, inplace=True)
+    trainset = train_df.to_dict(orient='records')
+    print("Loaded Fewshot Set:", trainset[:3])
+    
+    # 벡터화할 텍스트 생성 및 유니코드 정규화
+    to_vectorize = ["\n\n".join(normalize_string(value) for value in example.values()) for example in trainset]
+    
+    # 벡터화 및 FAISS DB 생성
+    fewshot_vectordb = FAISS.from_texts(to_vectorize, embedding=get_embedding(), metadatas=trainset)
+    
+    # FAISS DB 저장
+    save_faiss_db(fewshot_vectordb, db_path)
+    
+    return fewshot_vectordb
+
+def save_faiss_db(db, db_path):
+    db.save_local(db_path)
+    with open(db_path + '_metadata.pkl', 'wb') as f:
+        pickle.dump(db, f)
+
+def load_faiss_db(db_path):
+    db = FAISS.load_local(db_path, embeddings=get_embedding(), allow_dangerous_deserialization=True)
+    with open(db_path + '_metadata.pkl', 'rb') as f:
+        metadata = pickle.load(f)
+    return db, metadata
+
+def load_chunks_make_docdb(pdf_directory, db_path):
+    if os.path.exists(db_path) and os.path.exists(db_path + '_metadata.pkl'):
+        print("Loading FAISS DB from:", db_path)
+        db, metadata = load_faiss_db(db_path)
+        return db
+
+    print("Loading PDF files from:", pdf_directory)
+    documents = []
+
+    # PDF 파일들을 로드하여 분할
+    pdf_files = glob(os.path.join(pdf_directory, '*.pdf').replace('\\', '/'))
+    for pdf_file in pdf_files:
+        loader = PyPDFLoader(pdf_file)
+        pdf_documents = loader.load()
+        documents.extend(pdf_documents)
+    
+    # 유니코드 정규화
+    for doc in documents:
+        doc.page_content = normalize_string(doc.page_content)
+
+    # 분할된 텍스트를 벡터로 변환하여 FAISS DB에 저장
+    chunk_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=32)
+    chunks = chunk_splitter.split_documents(documents)
+    print("Done.", len(chunks), "chunks")
+    
+    print("Creating FAISS DB")
+    # FAISS DB 생성 및 저장
+    db = FAISS.from_documents(chunks, embedding=get_embedding())
+    save_faiss_db(db, db_path)
+    print("Done.")
+    
+    return db
+
+def make_db(df):
+    documents = []
+    pdf_files = df['Source_path'].unique()
+    print("Loading PDF files from:", len(pdf_files))
+    for pdf_file in pdf_files:
+        loader = PyPDFLoader(pdf_file)
+        pdf_documents = loader.load()
+        documents.extend(pdf_documents)
+    # 유니코드 정규화
+    for doc in documents:
+        doc.page_content = normalize_string(doc.page_content)
+    chunk_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=32)
+    chunks = chunk_splitter.split_documents(documents)
+    print("Done.", len(chunks), "chunks")
+    
+    print("Creating FAISS DB")
+    # FAISS DB 생성 및 저장
+    db = FAISS.from_documents(chunks, embedding=get_embedding())
+    print("Done.")
+    return db
+
+def make_fewshot_db(df):
+    df = df.drop('SAMPLE_ID', axis=1)
+    df = df.to_dict(orient='records')
+    # 벡터화할 텍스트 생성 및 유니코드 정규화
+    to_vectorize = ["\n\n".join(normalize_string(value) for value in example.values()) for example in df]
+    
+    # 벡터화 및 FAISS DB 생성
+    fewshot_vectordb = FAISS.from_texts(to_vectorize, embedding=get_embedding(), metadatas=df)
+    return fewshot_vectordb
--- run_bw.py
+++ run_bw.py
@@ -4,7 +4,7 @@ import pandas as pd
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
-from faiss_module import load_and_vectorize,load_chunks_make_docdb
+from faiss_module_bw import load_and_vectorize,load_chunks_make_docdb,create_multi_query_retriever
 from model_bw import setup_llm_pipeline
 from fewshot_module import fewshot_ex
 from save_module_bw import save
@@ -13,24 +13,28 @@ from utils_module import make_dict, extract_answer, format_docs
 seed_everything(52)
 
 def run(model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"):
+    llm = setup_llm_pipeline(model_id)
+
     # train에도 RAG를 쓸 때 사용
     train_db = load_chunks_make_docdb('./train_source', './train_faiss_db')
-    train_retriever = train_db.as_retriever(search_kwargs={'k': 1})
+    #train_retriever = train_db.as_retriever(search_kwargs={'k': 1})
+    train_retriever = create_multi_query_retriever(train_db, llm, k=1)
     # train_dict = make_dict('train.csv')
 
     test_db = load_chunks_make_docdb('./test_source', './test_faiss_db')
-    test_retriver = test_db.as_retriever(search_kwargs={'k': 3})
+    #test_retriver = test_db.as_retriever(search_kwargs={'k': 3})
+    test_retriver = create_multi_query_retriever(test_db, llm, k=3)
     test_dict = make_dict('test.csv')
 
     fewshot_db = load_and_vectorize('train.csv', './fewshot_faiss_db')
 
-    llm = setup_llm_pipeline(model_id)
+    
     # reordering = LongContextReorder()
     results =[]
     for i in tqdm(range(len(test_dict))):
         # train_retriever가 있으면 context를 포함한 fewshot prompt 생성
         # 없으면 fewshot prompt만 생성
-        fewshot_str = fewshot_ex(fewshot_db, test_dict[i],train_retriever= train_retriever, fewshot_num = 7)
+        fewshot_str = fewshot_ex(fewshot_db, test_dict[i],train_retriever= train_retriever, fewshot_num = 3)
         #print(fewshot_str)
 
         full_template = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
